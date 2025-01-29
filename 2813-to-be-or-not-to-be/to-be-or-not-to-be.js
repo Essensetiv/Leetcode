1db20bdc65d37520c9058132f72c065825a64d15
influence <163916433+Essensetiv@users.noreply.github.com>
2025-01-30 00:15:03 +0300

    Time: 52 ms (58.73%) | Memory: 49 MB (45.72%) - LeetSync

--- 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -3,23 +3,25 @@
  * @return {Object}
  */
 var expect = function(val) {
-        return{
-            toBe:(val2)=>{
-                if(val !== val2){
-                    throw new Error("Not Equal");
-                }else{
-                    return true;
-                }
-            },
-            notToBe:(val2)=>{
-                if(val === val2){
-                    throw new Error("Equal")
-                }else{
-                    return true;
-                }
+    return {
+        toBe: (val2) => {
+            if (val !== val2){
+                throw new Error("Not Equal");
+            }else{
+                return true;
+            }
+        },
+        notToBe: (val2) => {
+            if (val === val2){
+                throw new Error("Equal");
+            }
+            else{
+                return true;
             }
         }
-}
+    }
+};
+
 /**
  * expect(5).toBe(5); // true
  * expect(5).notToBe(5); // throws "Equal"
