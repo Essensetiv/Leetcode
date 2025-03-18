7ed0012b5ee48c3c3a6abd258d749859c99297bc
influence <163916433+Essensetiv@users.noreply.github.com>
2025-03-18 23:33:11 +0300

    Time: 2 ms (85.19%) | Memory: 62.8 MB (51.85%) - LeetSync

---
+++ 2478-longest-nice-subarray/longest-nice-subarray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestNiceSubarray = function(nums) {
+    let l = 0, usedBits = 0, maxLength = 0;
+
+    for (let r = 0; r < nums.length; r++) {
+        while ((usedBits & nums[r]) !== 0) {
+            usedBits ^= nums[l];
+            l++;
+        }
+
+        usedBits |= nums[r];
+        maxLength = Math.max(maxLength, r - l + 1);
+    }
+
+    return maxLength;
+};
